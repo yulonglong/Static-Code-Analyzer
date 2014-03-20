90561311c5be6281918b8d1efc69b63af2bd5e34
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-03-20 10:56:59 +0000

    Change some path.

--- PowerRangerMain/EmptyGeneralTesting/SPA/Follows.cpp
+++ PowerRangerMain/EmptyGeneralTesting/SPA/Follows.cpp
@@ -2,9 +2,6 @@
 
 #include "Follows.h"
 
-vector<STMTNUM> followsTable;
-vector<STMTNUM> followedByTable;
-
 // constructor
 Follows::Follows() {
 	// Note: program line 0 is not to be used for the enumeration of program lines
@@ -13,12 +10,12 @@ Follows::Follows() {
 }
 
 VOID Follows::setFollows(STMTNUM s1, STMTNUM s2) {
-	if (s1 >= followsTable.size()) {
+	if (s1 >= (signed int) followsTable.size()) {
 		followsTable.resize(s1+1, -1);
 	}
 	followsTable[s1] = s2;
 
-	if (s2 >= followedByTable.size()) {
+	if (s2 >= (signed int) followedByTable.size()) {
 		followedByTable.resize(s2+1, -1);
 	}
 	followedByTable[s2] = s1; 
