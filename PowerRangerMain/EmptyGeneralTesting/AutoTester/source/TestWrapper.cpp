091aa46fe23c87627a6247ba2c1223bfe37e034c
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-03-24 03:12:35 +0000

    Modified TestWrapper.cpp

--- PowerRangerMain/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
+++ PowerRangerMain/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
@@ -1,5 +1,8 @@
 #include "TestWrapper.h"
 
+VarTable varTable;
+ProcTable procTable;
+
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
 AbstractWrapper* WrapperFactory::createWrapper() {
@@ -13,20 +16,26 @@ volatile bool TestWrapper::GlobalStop = false;
 TestWrapper::TestWrapper() {
   // create any objects here as instance variables of this class
   // as well as any initialization required for your spa program
+	parse("Sample-Source.txt");
+
 }
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
 	// call your parser to do the parsing
-  // ...rest of your code...
+	// ...rest of your code...
+	parseCode(filename, varTable, procTable);
 }
 
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
+	// call your evaluator to evaluate the query here
+	// ...code to evaluate query...
 
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+
+	// store the answers to the query in the results list (it is initially empty)
+	// each result must be a string.
+
+
 }
 
