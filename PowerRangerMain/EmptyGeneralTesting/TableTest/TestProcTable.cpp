35426b9bc9215e95fcc98425366fa990fd525db0
Steven Kester Yuwono <a0080415@nus.edu.sg>
2014-03-26 09:26:36 +0000

    created unit testing for table, TableTest

---
+++ PowerRangerMain/EmptyGeneralTesting/TableTest/TestProcTable.cpp
@@ -0,0 +1,49 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "ProcTable.h"
+#include "TestProcTable.h"
+
+#include <iostream>
+#include <string>
+
+void 
+ProcTableTest::setUp()
+{
+}
+
+void 
+ProcTableTest::tearDown()
+{
+}
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( ProcTableTest ); // Note 4 
+
+// method to test the constructor
+void ProcTableTest::testConstructor()
+{  // Note 5
+	// create a student object
+	ProcTable procTable;
+	return;
+}
+
+// method to test the assigning and retrieval of grades
+void ProcTableTest::testTableSet()
+{
+	// create a student
+	ProcTable procTable;
+
+	// assign a few grades to this student
+	procTable.insertProc("x");
+	procTable.insertProc("y");
+	procTable.insertProc("z");
+
+	// verify that the assignment is correct - Note 7
+	CPPUNIT_ASSERT_EQUAL(0, procTable.getProcIndex("x"));
+	CPPUNIT_ASSERT_EQUAL(1, procTable.getProcIndex("y"));
+	CPPUNIT_ASSERT_EQUAL(2, procTable.getProcIndex("z"));
+
+	// attempt to retrieve a course that does not exist
+	CPPUNIT_ASSERT_EQUAL(-1, procTable.getProcIndex("a"));
+
+	return;
+}
