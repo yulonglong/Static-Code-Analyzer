fd8e188602e25ca42a0e20564fae22ebd02c5fa3
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-03-31 08:58:21 +0000

    Updated AutoTester

--- PowerRangerMain/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
+++ PowerRangerMain/EmptyGeneralTesting/AutoTester/source/TestWrapper.cpp
@@ -1,4 +1,6 @@
 #include "TestWrapper.h"
+// #include "QueryProcessor.h"
+#include <vector>
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -21,11 +23,16 @@ void TestWrapper::parse(std::string filename) {
   // ...rest of your code...
 }
 
-// method to evaluating a query
+// method to evaluating a query	
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
+/*
+Query parsedQuery = parse(query);
+	vector<int> answers = evaluateQuery(parsedQuery);
 
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+	for(vector<int>::iterator it = answers.begin(); it!=answers.end(); it++){
+		int currAnswer = it->getToken1();
+		string str = static_cast<ostringstream*>( &(ostringstream() << currAnswer) )->str();
+		results.push_back(str);
+	}
+*/
 }
