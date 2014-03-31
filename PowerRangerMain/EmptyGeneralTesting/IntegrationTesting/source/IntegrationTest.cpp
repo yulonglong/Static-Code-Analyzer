cc3dda4a7ab94d5a6263885abda0cf0c2c71942d
Steven Kester Yuwono <a0080415@nus.edu.sg>
2014-03-31 15:05:40 +0000

    included integration test

--- PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
+++ PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
@@ -10,47 +10,14 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-// Get the top level suite from the registry
-CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
-CppUnit::TextUi::TestRunner runner;
+	// Get the top level suite from the registry
+	CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+	CppUnit::TextUi::TestRunner runner;
 
-//parse the SIMPLE soource code into the parser
-//Parse();
+	runner.addTest(suite);
+	bool wasSucessful = runner.run();
 
-//Call of DesignExtractor
-//Extract();
+	getchar();
 
-string fileName = "Source1"; 
-VarTable varTable; 
-ProcTable procTable;
-
-Parse(fileName, varTable, procTable);
-
-string s = "assign a; Select a;";
-
-ParseAndEvaluate(s);
-
-// ouput result to screen 
-
-runner.addTest(suite);
-bool wasSucessful = runner.run();
-
-//getchar();
-
-return wasSucessful ? 0 : 1;
-}
-
-
-void Parse(string fileName, VarTable &varTable, ProcTable &procTable) {
-	//parse the SIMPLE source code into the parser
-	parseCode(fileName, varTable, procTable);
-}
-
-void Extract() {
-}
-
-// TODO: include evaluation methods. 
-void ParseAndEvaluate(string s) {
-	// QueryParser qp;
-	// Query query = qp.parse(s);
+	return wasSucessful ? 0 : 1;
 }
---
+++ PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/TestIntegrate.cpp
@@ -0,0 +1,177 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "Node.h"
+#include "CodeParser.h"
+#include "Testintegrate.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void 
+IntegrateTest::setUp()
+{
+}
+
+void IntegrateTest::tearDown()
+{
+}
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( IntegrateTest ); // Note 4 
+
+// method to test the constructor
+void IntegrateTest::testModifyTable()
+{  // Note 5
+	VarTable varTable;
+	ProcTable procTable;
+	Node* root = parseCode("CodeParserTestIn.txt",varTable,procTable);
+
+	string expected = "i";
+	CPPUNIT_ASSERT_EQUAL(expected, varTable.getVarName(0));
+	expected = "x";
+	CPPUNIT_ASSERT_EQUAL(expected, varTable.getVarName(1));
+	expected = "y";
+	CPPUNIT_ASSERT_EQUAL(expected, varTable.getVarName(2));
+	expected = "z";
+	CPPUNIT_ASSERT_EQUAL(expected, varTable.getVarName(3));
+
+	expected = "First";
+	CPPUNIT_ASSERT_EQUAL(expected, procTable.getProcName(0));
+	expected = "Second";
+	CPPUNIT_ASSERT_EQUAL(expected, procTable.getProcName(1));
+	expected = "Third";
+	CPPUNIT_ASSERT_EQUAL(expected, procTable.getProcName(2));
+	return;
+}
+
+// method to test the assigning and retrieval of grades
+void IntegrateTest::testCompleteParser()
+{
+	// create a node
+	VarTable varTable;
+	ProcTable procTable;
+	Node* root = parseCode("CodeParserTestIn.txt",varTable,procTable);
+	// assign
+	Node* curr = root;
+	curr = root->getChild()[0];
+	curr = curr->getChild()[0];
+	curr = curr->getChild()[0];
+
+	string expected = "=";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "assign";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+
+	expected = "x";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "variable";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	// verify that the assignment is correct - Note 7
+	
+
+	return;
+}
+
+void IntegrateTest::testParserSource1()
+{
+	VarTable varTable;
+	ProcTable procTable;
+	Node* root = parseCode("Source1.txt",varTable,procTable);
+
+	Node* curr = root;
+	
+	string expected = "root";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "program";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+	expected = "Mini";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "procedure";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+    expected = "Mini";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "stmtLst";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+    expected = "=";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "assign";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+	expected = "A1";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "variable";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+    curr = root;
+
+	curr = curr->getChild()[1];
+	expected = "mini";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "procedure";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+    expected = "mini";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "stmtLst";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[1];
+    expected = "=";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "assign";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[1];
+	expected = "A1";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "variable";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	return;
+}
+
+void IntegrateTest::testParserSource2()
+{
+	VarTable varTable;
+	ProcTable procTable;
+	Node* root = parseCode("Source2.txt",varTable,procTable);
+
+	Node* curr = root;
+	
+	string expected = "root";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "program";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+	expected = "ABC";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "procedure";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[0];
+    expected = "ABC";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	expected = "stmtLst";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	curr = curr->getChild()[3];
+    expected = "4";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getData());
+	CPPUNIT_ASSERT_EQUAL(4, curr->getProgLine());
+	expected = "while";
+	CPPUNIT_ASSERT_EQUAL(expected, curr->getType());
+
+	return;
+}
