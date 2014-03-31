f7cb36a65264c6e95ebfb78d5c43f2932fafbc29
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-03-31 11:41:38 +0000

    Modified IntegrationTest.cpp

--- PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
+++ PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/IntegrationTest.cpp
@@ -1,7 +1,10 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include <iostream>
-
+#include <CodeParser.h>
+#include <VarTable.h>
+#include <ProcTable.h>
+// #include <QueryParser.h>
 
 using namespace std;
 
@@ -17,11 +20,37 @@ CppUnit::TextUi::TestRunner runner;
 //Call of DesignExtractor
 //Extract();
 
+string fileName = "Source1"; 
+VarTable varTable; 
+ProcTable procTable;
+
+Parse(fileName, varTable, procTable);
+
+string s = "assign a; Select a;";
+
+ParseAndEvaluate();
+
+// ouput result to screen 
+
 runner.addTest(suite);
 bool wasSucessful = runner.run();
-
 
 //getchar();
 
 return wasSucessful ? 0 : 1;
+}
+
+
+void Parse(string fileName, VarTable &varTable, ProcTable &procTable) {
+	//parse the SIMPLE source code into the parser
+	parseCode(fileName, varTable, procTable);
+}
+
+void Extract() {
+}
+
+// TODO: include evaluation methods. 
+void ParseAndEvaluate(string s) {
+	// QueryParser qp;
+	// Query query = qp.parse(s);
 }
