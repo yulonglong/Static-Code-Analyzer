6e987c63ea28136df30d182bd3c904a0609c600b
Steven Kester Yuwono <a0080415@nus.edu.sg>
2014-04-01 13:17:30 +0000

    added thorough testing for parser

--- PowerRangerMain/EmptyGeneralTesting/CodeParserTest/TestParser.h
+++ PowerRangerMain/EmptyGeneralTesting/CodeParserTest/TestParser.h
@@ -11,6 +11,9 @@ class ParserTest : public CPPUNIT_NS::TestFixture // Note 2
 	CPPUNIT_TEST( testCompleteParser );
 	CPPUNIT_TEST( testParserSource1 );
 	CPPUNIT_TEST( testParserSource2 );
+	CPPUNIT_TEST( testFullParserOwnSource );
+	CPPUNIT_TEST( testFullParserSource1 );
+	CPPUNIT_TEST( testFullParserSource2 );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -24,6 +27,9 @@ public:
 	void testCompleteParser();
 	void testParserSource1();
 	void testParserSource2();
+	void testFullParserOwnSource();
+	void testFullParserSource1();
+	void testFullParserSource2();
 };
 #endif
     
