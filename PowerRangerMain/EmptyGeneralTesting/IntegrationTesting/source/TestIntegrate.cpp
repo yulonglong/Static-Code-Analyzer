7e5696b4f387f1ad823821cacb817d64bee7f23b
adinda <adinda@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-01 04:03:18 +0000

    Added codes from query parser. Problems with including the header file.

--- PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/TestIntegrate.cpp
+++ PowerRangerMain/EmptyGeneralTesting/IntegrationTesting/source/TestIntegrate.cpp
@@ -2,7 +2,7 @@
 #include "Node.h"
 #include "CodeParser.h"
 #include "Testintegrate.h"
-// #include <QueryParser.h>
+// #include "QueryParser.h"
 
 
 #include <iostream>
@@ -180,9 +180,44 @@ void IntegrateTest::testParserSource2()
 
 // void IntegrateTest::testPQLSource1() {
 	// testParseSource1();
-	string s;
+
+	string s1 = "assign a; Select a such that Follows(a, 7)";
+	string s2 = "assign a; Select a such that Follows(3, a)";
+	string s3 = "stmt s; Select s such that Follows(4, s)";
+	string s4 = "Select BOOLEAN such that Follows(6, 7)";
+	string s5 = "Select BOOLEAN such that Follows(5, 6)";
+
+	string s6 = "while w; Select w such that Follows(3, w)";
+	string s7 = "while w; Select w such that Follows(4, w)";
+	string s8 = "stmt s; Select s such that Follows(s, 19)";
+	string s9 = "Select BOOLEAN such that Follows(15, 16)";
+	string s10 = "stmt s; Select s such that Parent(s, 9)";
+	string s11 = "while w; Select w such that Parent(w, 11)";
+	string s12 = "assign a; Select a such that Parent(a, 3)";
+	string s13 = "while w; assign a; Select w such that Parent(w, a)";
+	string s14 = "Select BOOLEAN such that Parent(4, 9)";
+	string s15 = "while w; Select BOOLEAN such that Parent(4, w)";
+	string s16 = "assign a; Select a such that Parent(11, a)";
 
 	// TODO: run query parser;
+	/*QueryParser qp;
+
+	Query q1 = qp.parse(s1);
+	Query q2 = qp.parse(s2);
+	Query q3 = qp.parse(s3);
+	Query q4 = qp.parse(s4);
+	Query q5 = qp.parse(s5);
+	Query q6 = qp.parse(s6);
+	Query q7 = qp.parse(s7);
+	Query q8 = qp.parse(s8);
+	Query q9 = qp.parse(s9);
+	Query q10 = qp.parse(s10);
+	Query q11 = qp.parse(s11);
+	Query q12 = qp.parse(s12);
+	Query q13 = qp.parse(s13);
+	Query q14 = qp.parse(s14);
+	Query q15 = qp.parse(s15);
+	Query q16 = qp.parse(s16);*/
 
 	// TODO: run evaluate on query
 
