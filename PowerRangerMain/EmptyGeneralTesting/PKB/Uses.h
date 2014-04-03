983fde0ea9aa6fe597d53efbbba77a79995ff817
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-03 10:30:33 +0000

    Changed method headers for Modifies.h and Uses.h.

--- PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
+++ PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
@@ -6,15 +6,23 @@
 #include "TypeTable.h"
 
 using namespace std;
+#ifndef MODIFIES_H
+#define MODIFIES_H
+
+#pragma once
+#include <iostream>
+#include "TypeTable.h"
+
 typedef int STMTNUM;
+using namespace std;
 
 class Uses {
 public:
 	Uses();
 	bool isUses(STMTNUM, string);	//Select w such that Uses(1, "y")
-	bool isUses(TypeTable::SynType, TypeTable::SynType, TypeTable::SynType);	//Select s such that Uses(a, v) parameter order: selected type, first token, second token
+	bool isUses(TypeTable::SynType, TypeTable::SynType);	//Select s such that Uses(a, v) parameter order: selected type, first token, second token
 	vector<int> getUses(TypeTable::SynType, string);	//Select a such that Uses(a, "x")	return -1 if doesn't exist
-	vector<string> getModified(TypeTable::SynType);		//Select v such that Uses(1, v)	return -1 if doesnt exist
+	vector<string> getModified(TypeTable::SynType);		//Select v such that Uses(1, v)	return empty vector if doesnt exist
 };
 
 #endif
