3f42732c1e085eac028cc75ba45e2100ba5a4d43
yohanes <yohanes@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-05 11:00:33 +0000

    Uses Implement halfway

--- PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
+++ PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
@@ -11,9 +11,11 @@ using namespace std;
 class Uses {
 private:
 	vector<vector<STMTNUM>> usesTable;
+	VarTable varTable;
 public:
-	Uses();
-	bool isUses(STMTNUM, VARNAME);	//Select w such that Uses(1, "y")
+	Uses(VarTable);
+	void setUses(STMTNUM, VARNAME);
+	bool isUses(STMTNUM, VARNAME);	//Select boolean such that Uses(1, "y")
 	bool isUses(TypeTable::SynType, TypeTable::SynType);	//Select s such that Uses(a, v) parameter order: selected type, first token, second token
 	vector<int> getUses(TypeTable::SynType, string);	//Select a such that Uses(a, "x")	return -1 if doesn't exist
 	vector<string> getModified(TypeTable::SynType);		//Select v such that Uses(1, v)	return empty vector if doesnt exist
