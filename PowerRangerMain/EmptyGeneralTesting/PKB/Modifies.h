aaaa1aa7ce6e887a4ce3d492fa4aed089537e769
ipsita <ipsita@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-05 16:19:04 +0000

    Added Modifies.

--- PowerRangerMain/EmptyGeneralTesting/PKB/Modifies.h
+++ PowerRangerMain/EmptyGeneralTesting/PKB/Modifies.h
@@ -2,18 +2,33 @@
 #define MODIFIES_H
 
 #pragma once
-#include <iostream>
-#include "TypeTable.h"
 
-using namespace std;
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include "TypeTable.h"
+#include "VarTable.h"
+
+typedef TypeTable::SynType TYPE; 
+
 
 class Modifies {
-public:
-	Modifies();
-	bool isModifies(STMTNUM, string);	//Select w such that Modifies(1, "y")
-	bool isModifies(TypeTable::SynType, TypeTable::SynType);	//Select s such that Modifies(a, v) parameter order: selected type, first token, second token (first parameter can be w or a or s)
-	vector<int> getModifies(TypeTable::SynType, string);	//Select a such that Modifies(a, "x")	return -1 if doesn't exist
-	vector<string> getModified(TypeTable::SynType);		//Select v such that Modifies(1, v)	return empty vector if doesnt exist };
+private:
+	vector<vector<STMTNUM>> modifiesTable; 
+	TypeTable typeTable; 
+	VarTable varTable; 
+
+public:	
+	Modifies(TypeTable, VarTable);
+	~Modifies();
+
+	void setModifies(STMTNUM, VARNAME);
+	bool isModifies(STMTNUM, VARNAME);	//Select w such that Modifies(1, "y")
+	bool isModifies(TYPE, TYPE);		//Select s such that Modifies(a, v) parameter order: selected type, first token, second token (first parameter can be w or a or s)
+	vector<STMTNUM> getModifies(TYPE, VARNAME);	//Select a such that Modifies(a, "x")	return empty vector if doesnt exist
+	vector<VARNAME> getModifies(STMTNUM);		//Select v such that Modifies(1, v)	return empty vector if doesnt exist
 };
 
 #endif
