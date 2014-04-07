c1eedfb890d2de4af1052e71b94a85b19f49b915
yohanes <yohanes@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-07 05:58:34 +0000

    Uses Implemented

--- PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
+++ PowerRangerMain/EmptyGeneralTesting/PKB/Uses.h
@@ -13,12 +13,13 @@ private:
 	vector<vector<STMTNUM>> usesTable;
 	VarTable varTable;
 public:
-	Uses(VarTable);
+	Uses(VarTable,TypeTable);
+	TypeTable typeTable; // for the sake of unit testing.
 	Uses();
 	void setUses(STMTNUM, VARNAME);
 	bool isUses(STMTNUM, VARNAME);	//Select boolean such that Uses(1, "y")
 	bool isUses(TypeTable::SynType, TypeTable::SynType);	//Select s such that Uses(a, v) parameter order: selected type, first token, second token
-	vector<int> getUses(TypeTable::SynType, string);	//Select a such that Uses(a, "x")	return -1 if doesn't exist
+	vector<int> getUses(TypeTable::SynType, VARNAME);	//Select a such that Uses(a, "x")	return -1 if doesn't exist
 	vector<string> getModified(TypeTable::SynType);		//Select v such that Uses(1, v)	return empty vector if doesnt exist
 };
 
