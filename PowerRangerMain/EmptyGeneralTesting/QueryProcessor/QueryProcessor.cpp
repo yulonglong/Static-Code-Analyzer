27f671e89fdfc78999dcf69bc40d7011e354afbf
hisyamni <hisyamni@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-07 08:51:55 +0000

    Printing log statements for PQL

--- PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
+++ PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
@@ -1,24 +1,32 @@
 #pragma once
 
 #include "QueryProcessor.h"
+#include "iostream"
 using namespace std;
 
 
 void queryDriver(string query, list<string> &result, PKB &pkb){
+	cout<<"Begin Parse query"<<endl;
 	QueryParser qp;
 	QueryEvaluator qe = QueryEvaluator::QueryEvaluator(pkb);
 	Query parsedQuery = qp.parse(query);
 
 	if(parsedQuery.getSelectedSyn() == "BOOLEAN") {
+		cout<<"End Parse query"<<endl;
+		cout<<"Begin Evaluate query"<<endl;
 		bool ans = qe.evaluateQueryBoolean(parsedQuery);
 		if(ans)
 			result.push_back("true");
 		else
 			result.push_back("false");
+		cout<<"End Evaluate query"<<endl;
 	}
 	else {
+		cout<<"End Parse query"<<endl;
+		cout<<"Begin Evaluate query"<<endl;
 		vector<int> ans = qe.evaluateQuery(parsedQuery);
 		for(int i=0; i<ans.size(); i++)
 			result.push_back(to_string(static_cast<long long>(ans.at(i))));
+		cout<<"End Evaluate query"<<endl;
 	}
 }
