dadf664e7986a9018a012b8dd4addca3df6e9352
hisyamni <hisyamni@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-12 10:58:37 +0000

    Updated Query Processor to output nothing when no result

--- PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
+++ PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
@@ -11,30 +11,27 @@ void queryDriver(string query, list<string> &result, PKB *pkb){
 	QueryEvaluator qe = QueryEvaluator::QueryEvaluator(pkb);
 
 	bool isValid = qp.validate(query);
-	//if (isValid) {
-	if(true){	
+	if (isValid) {
 		Query parsedQuery = qp.parse();
-		//if(parsedQuery.getSelectedSyn() == "BOOLEAN") {
-		/*if(false){	
-		cout<<"End Parse query"<<endl;
-		cout<<"Begin Evaluate query"<<endl;
-		bool ans = qe.evaluateQueryBoolean(parsedQuery);
-		if(ans)
-		result.push_back("true");
-		else
-		result.push_back("false");
-		cout<<"End Evaluate query"<<endl;
+		if(parsedQuery.getSelectedSyn() == "BOOLEAN") {
+			cout<<"End Parse query"<<endl;
+			cout<<"Begin Evaluate query"<<endl;
+			bool ans = qe.evaluateQueryBoolean(parsedQuery);
+			if(ans)
+				result.push_back("true");
+			else
+				result.push_back("false");
+			cout<<"End Evaluate query"<<endl;
 		}
-		else {*/
-
-
-		cout<<"End Parse query"<<endl;
-		cout<<"Begin Evaluate query"<<endl;
-		cout<< "autoteste 333 size " << pkb->getFollows()->followedByTable.size()<<endl;
-		vector<int> ans = qe.evaluateQuery(parsedQuery);
-		for(size_t i = 0; i < ans.size(); i++)
-			result.push_back(to_string(static_cast<long long>(ans.at(i))));
-		cout<<"End Evaluate query"<<endl;
+		else {
+			cout<<"End Parse query"<<endl;
+			cout<<"Begin Evaluate query"<<endl;
+			cout<< "autoteste 333 size " << pkb->getFollows()->followedByTable.size()<<endl;
+			vector<int> ans = qe.evaluateQuery(parsedQuery);
+			for(size_t i = 0; i < ans.size(); i++)
+				if(ans.at(i) != -1)
+					result.push_back(to_string(static_cast<long long>(ans.at(i))));
+			cout<<"End Evaluate query"<<endl;
 		
 	}
 }
