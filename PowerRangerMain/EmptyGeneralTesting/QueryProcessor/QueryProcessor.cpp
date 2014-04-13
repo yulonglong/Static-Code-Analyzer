0d048dfe29d5b5bfc59382d82110c54556c9866a
hisyamni <hisyamni@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-13 05:34:03 +0000

    Modified Query Processor to return variable names on demand, fixed answers to q3-1

--- PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
+++ PowerRangerMain/EmptyGeneralTesting/QueryProcessor/QueryProcessor.cpp
@@ -13,24 +13,30 @@ void queryDriver(string query, list<string> &result, PKB *pkb){
 	bool isValid = qp.validate(query);
 	if (isValid) {
 		Query parsedQuery = qp.parse();
+		cout<<"End Parse query"<<endl;
+		cout<<"Begin Evaluate query"<<endl;
 		if(parsedQuery.getSelectedSyn() == "BOOLEAN") {
-			cout<<"End Parse query"<<endl;
-			cout<<"Begin Evaluate query"<<endl;
 			bool ans = qe.evaluateQueryBoolean(parsedQuery);
 			if(ans)
 				result.push_back("true");
 			else
 				result.push_back("false");
-			cout<<"End Evaluate query"<<endl<<endl<<endl<<endl;
 		}
 		else {
-			cout<<"End Parse query"<<endl;
-			cout<<"Begin Evaluate query"<<endl;
 			vector<int> ans = qe.evaluateQuery(parsedQuery);
+			if(parsedQuery.getSelectedSyn().compare("v")==0) {
+				VarTable* varTable = pkb->getVarTable();
+
+				for(size_t i = 0; i < ans.size(); i++)
+					if(ans.at(i) != -1)
+						result.push_back(varTable->getVarName(ans.at(i)));
+			}
+			else {
 			for(size_t i = 0; i < ans.size(); i++)
 				if(ans.at(i) != -1)
 					result.push_back(to_string(static_cast<long long>(ans.at(i))));
-			cout<<"End Evaluate query"<<endl;
-		}	
+			}
+		}
+		cout<<"End Evaluate query"<<endl;
 	}
 }
