1c4af03b6cb49a2c3473d42fc91e674fdce12dab
fanyuxin <fanyuxin@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-15 13:25:46 +0000

    updated qe unit testing

--- PowerRangerMain/EmptyGeneralTesting/QueryEvaluatorTest/QueryEvaluatorTest.h
+++ PowerRangerMain/EmptyGeneralTesting/QueryEvaluatorTest/QueryEvaluatorTest.h
@@ -8,6 +8,8 @@ class QueryEvaluatorTest: public CPPUNIT_NS::TestFixture
 	CPPUNIT_TEST_SUITE( QueryEvaluatorTest );
 	CPPUNIT_TEST( testEvaluateFollows );
 	CPPUNIT_TEST( testEvaluateParent );
+	CPPUNIT_TEST( testEvaluateModifies );
+	CPPUNIT_TEST( testEvaluateUses );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -16,6 +18,9 @@ public:
 
 	void testEvaluateFollows();
 	void testEvaluateParent();
+	void testEvaluateModifies();
+	void testEvaluateUses();
 
 };
+
 #endif
