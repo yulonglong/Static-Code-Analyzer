c96313f9e89cabe963d5d805a42e9b993ad4ccae
yohanes <yohanes@ab629649-b387-4ad0-954b-8d964a7fb398>
2014-04-16 15:39:59 +0000

    Change uses from map to vector

--- PowerRangerMain/EmptyGeneralTesting/PKB/Uses.cpp
+++ PowerRangerMain/EmptyGeneralTesting/PKB/Uses.cpp
@@ -7,6 +7,8 @@ Uses* Uses::uses=NULL;
 
 //Constructor
 Uses::Uses(){
+	vector<VARINDEX> temp (1,-1);
+	usesTable.assign(1,temp);
 }
 
 Uses::~Uses(){
@@ -44,22 +46,23 @@ Uses* Uses::getInstance(TypeTable* tt, VarTable* vt) {
 Uses::Uses(TypeTable *tt,VarTable *vt){
 	typeTable = tt;
 	varTable = vt;
+	vector<VARINDEX> temp (1,-1);
+	usesTable.assign(1,temp);
 }
 
 void Uses::setUses(STMTNUM s,VARNAME v){
 	try{
 		VARINDEX index = varTable->getVarIndex(v);
 
-		vector<STMTNUM> existing;
-		try{
-			existing = usesTable.at(s);
-			usesTable.erase(s);
+		vector<VARINDEX> temp (1,-1);
+		if (s >= (signed int) usesTable.size()) {
+			usesTable.resize(s+1, temp);
 		}
-		catch(...){
-		}
-		existing.push_back(index);
-		usesTable.insert(pair<STMTNUM,vector<STMTNUM>>(s,existing));
-		cout<<"setUses("<<s<<","<<v<<")"<<" var Index = "<<index<<endl;
+		vector<VARINDEX> temp1 = usesTable.at(s);
+		if(temp1==temp)
+			temp1.clear();
+		temp1.push_back(index);
+		usesTable[s] = temp1;
 	}catch(...){
 	}
 }
@@ -67,8 +70,8 @@ void Uses::setUses(STMTNUM s,VARNAME v){
 bool Uses::isUses(STMTNUM s, VARNAME v){
 	try{
 		VARINDEX index = varTable->getVarIndex(v);
-		vector<STMTNUM> temp = usesTable.at(s);
-		vector<STMTNUM>::iterator it = temp.begin();
+		vector<VARINDEX> temp = usesTable.at(s);
+		vector<VARINDEX>::iterator it = temp.begin();
 		for(;it!=temp.end();it++){
 			if(index==*it)
 				return true;
@@ -79,39 +82,40 @@ bool Uses::isUses(STMTNUM s, VARNAME v){
 	}
 }
 
-vector<int> Uses::getUses(TypeTable::SynType t, VARNAME v){	//Select a such that Uses(a, "x")	return -1 if doesn't exist
+vector<int> Uses::getUses(TYPE t, VARNAME v){	//Select a such that Uses(a, "x")	return -1 if doesn't exist
 	try{
-
 		VARINDEX index = varTable->getVarIndex(v);
-		vector<STMTNUM> ans;
-		for(map<STMTNUM,vector<STMTNUM>>::iterator i = usesTable.begin(); i != usesTable.end(); i++){
-			vector<STMTNUM> temp = i->second;
-			vector<STMTNUM>::iterator it = temp.begin();
+		vector<VARINDEX> ans;
+		for(size_t i=0;i<usesTable.size();i++){
+			vector<VARINDEX> temp = usesTable.at(i);
+			vector<VARINDEX>::iterator it = temp.begin();
 			for(;it!=temp.end();++it){
 				if(*it==index){
 					if(t==TypeTable::STMT){
-						ans.push_back(i->first);
+						ans.push_back(i);
 						break;
 					}
-					else if(typeTable->isType(t,i->first)){
-						ans.push_back(i->first);
+					else if(typeTable->isType(t,i)){
+						ans.push_back(i);
 						break;
 					}
 				}
 			}
 		}
 		if(ans.empty())
-			return vector<int> (1,-1);
+			return vector<VARINDEX> (1,-1);
 		else
 			return ans;
 	}catch(...){
-		return vector<int> (1,-1);
+		return vector<VARINDEX> (1,-1);
 	}
 }
 
 vector<VARINDEX> Uses::getUses(STMTNUM s){	//Select v such that Uses(1, v)	return variable indexes. otherwise return empty vector if doesnt exist
 	try{
 		vector<VARINDEX> temp = usesTable.at(s);
+		if(temp==vector<VARINDEX> (1,-1))
+			temp.clear();
 		return temp;
 	}catch(...){
 		vector<VARINDEX> temp;
@@ -119,15 +123,17 @@ vector<VARINDEX> Uses::getUses(STMTNUM s){	//Select v such that Uses(1, v)	retur
 	}
 }
 
-vector<VARINDEX> Uses::getUses(TypeTable::SynType type){	//Select a such that Uses(a, v); return empty vector if does not exist
+vector<VARINDEX> Uses::getUses(TYPE type){	//Select a such that Uses(a, v); return empty vector if does not exist
 	try{
 		vector<VARINDEX> ans;
-		for(map<STMTNUM,vector<STMTNUM>>::iterator i = usesTable.begin(); i != usesTable.end(); i++){
-			if(!i->second.empty()){
-				if(typeTable->isType(type,i->first))
-					ans.push_back(i->first);
+		for(size_t i=0;i<usesTable.size();i++){
+			if(!usesTable.at(i).empty() && usesTable.at(i)!=vector<int> (1,-1)){
+				if(typeTable->isType(type,i))
+					ans.push_back(i);
 			}
 		}
+		if(ans==vector<int> (1,-1))
+			ans.clear();
 		return ans;
 	}catch(...){
 		vector<VARINDEX> ans;
