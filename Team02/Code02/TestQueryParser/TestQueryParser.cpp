5dbcf1c514b2def104f64977ad967b424273e252
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-08-20 01:09:38 +0800

    added test case for TestQueryParser

--- Team02/Code02/TestQueryParser/TestQueryParser.cpp
+++ Team02/Code02/TestQueryParser/TestQueryParser.cpp
@@ -1,5 +1,6 @@
 #include <cppunit/config/SourcePrefix.h>
-#include "Node.h"
+#include "Relationship.h"
+#include "Query.h"
 #include "QueryParser.h"
 #include "TestQueryParser.h"
 #include "PKB.h"
@@ -10,21 +11,52 @@
 using namespace std;
 
 void 
-QueryParserTest::setUp()
-{
+QueryParserTest::setUp(){
 }
 
 void 
-QueryParserTest::tearDown()
-{
+QueryParserTest::tearDown(){
 }
 
 // Registers the fixture into the 'registry'
-CPPUNIT_TEST_SUITE_REGISTRATION( QueryParserTest ); // Note 4 
+CPPUNIT_TEST_SUITE_REGISTRATION( QueryParserTest );
 
-// method to test TABLE
-void QueryParserTest::testModifyTable()
-{  // Note 5
+void QueryParserTest::testQueryAssignSelectModifies(){
+	string query = "assign a; Select a such that Modifies(a, \"oSCar\")";
+	QueryParser qp;
+	bool isValid = true;
+	Query parsedQuery = qp.parse(query,isValid);
 	
+	string selectedSyn = parsedQuery.getSelectedSyn();
+	string patternSyn = parsedQuery.getPatternSyn();
+	vector<Relationship> relVect = parsedQuery.getRelVect();
+	unordered_map<string, TypeTable::SynType> synTable = parsedQuery.getSynTable();
+
+	unordered_map<string, TypeTable::SynType>::iterator iter;
+	iter= synTable.begin();
+
+	string expectedSelectedSyn = "a";
+	string expectedPatternSyn = "";
+	vector<Relationship> expectedRelVect;
+	Relationship tempRel("Modifies","a","\"oSCar\"");
+	expectedRelVect.push_back(tempRel);
+	unordered_map<string, TypeTable::SynType> expectedSynTable;
+	expectedSynTable.insert(make_pair("BOOLEAN", TypeTable::BOOLEAN));
+	expectedSynTable.insert(make_pair("a", TypeTable::ASSIGN));
+
+	unordered_map<string, TypeTable::SynType>::iterator expectedIter;
+	expectedIter= expectedSynTable.begin();
+
+	CPPUNIT_ASSERT_EQUAL(expectedSelectedSyn,selectedSyn);
+	CPPUNIT_ASSERT_EQUAL(expectedPatternSyn,patternSyn);
+	CPPUNIT_ASSERT_EQUAL(expectedRelVect[0].getRelType(),relVect[0].getRelType());
+	CPPUNIT_ASSERT_EQUAL(expectedRelVect[0].getToken1(),relVect[0].getToken1());
+	CPPUNIT_ASSERT_EQUAL(expectedRelVect[0].getToken2(),relVect[0].getToken2());
+	CPPUNIT_ASSERT_EQUAL(expectedIter->first,iter->first);
+	CPPUNIT_ASSERT_EQUAL(expectedIter->second,iter->second);
+	iter++; expectedIter++;
+	CPPUNIT_ASSERT_EQUAL(expectedIter->first,iter->first);
+	CPPUNIT_ASSERT_EQUAL(expectedIter->second,iter->second);
+
 	return;
 }
--- Team02/Code02/TestQueryParser/TestQueryParser.h
+++ Team02/Code02/TestQueryParser/TestQueryParser.h
@@ -1,21 +1,19 @@
-#ifndef TestParser_h
-#define TestParser_h
+﻿#ifndef TestQueryParser_h
+#define TestQueryParser_h
 
 // Note 1
 #include <cppunit/extensions/HelperMacros.h>
 
-class QueryParserTest : public CPPUNIT_NS::TestFixture // Note 2 
-{ 
-	CPPUNIT_TEST_SUITE( QueryParserTest ); // Note 3 
-	CPPUNIT_TEST( testModifyTable );
+class QueryParserTest : public CPPUNIT_NS::TestFixture{ 
+	CPPUNIT_TEST_SUITE( QueryParserTest );
+	CPPUNIT_TEST(testQueryAssignSelectModifies);
 	CPPUNIT_TEST_SUITE_END();
 
 public:
 	void setUp();
 	void tearDown();
 
-	// method to test the constructor
-	void testModifyTable();
+	void testQueryAssignSelectModifies();
 };
 #endif
     
