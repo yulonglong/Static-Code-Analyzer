031ce171a05338fc9e34c1c56e323bf8138c2ace
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-08-26 18:25:01 +0800

    Refactored QueryParser

--- Team02/Code02/QueryProcessor/QueryParser.cpp
+++ Team02/Code02/QueryProcessor/QueryParser.cpp
@@ -6,22 +6,44 @@
 #include <sstream>
 using namespace std;
 
-QueryParser::QueryParser(){
-	varName = "([A-Za-z][A-Za-z0-9#_]*)";
-	select = "([Ss]elect)";
-	suchThat = "(such)\\s+(that)";
-	freeString = "(\\S+)";
-	designEntity = "(procedure|stmtLst|stmt|assign|call|while|if|variable|constant|prog_line|plus|minus|times)";
-	relRef = "([Ff]ollows|[Ff]ollows\\*|[Mm]odifies|[Mm]odifies\\*|[Uu]ses|[Uu]ses\\*|[Pp]arent|[Pp]arent\\*|[Cc]alls|[Cc]alls\\*|[Nn]ext|[Nn]ext\\*|[Aa]ffects|[Aa]ffects\\*)";
-	pattern = "(pattern)";
+//constant string
+const string QueryParser::MODIFIES = "modifies";
+const string QueryParser::USES = "uses";
+const string QueryParser::CALLS = "calls";
+const string QueryParser::CALLSSTAR = "calls*";
+const string QueryParser::PARENT = "parent";
+const string QueryParser::PARENTSTAR = "parent*";
+const string QueryParser::FOLLOWS = "follows";
+const string QueryParser::FOLLOWSSTAR = "follows*";
+const string QueryParser::NEXT = "next";
+const string QueryParser::NEXTSTAR = "next*";
+const string QueryParser::AFFECTS = "affects";
+const string QueryParser::AFFECTSSTAR = "affects*";
 
+//constant string for regex pattern
+const string QueryParser::IDENT = "([A-Za-z][A-Za-z0-9#]*)";
+const string QueryParser::select = "([Ss]elect)";
+const string QueryParser::suchThat = "(such)\\s+(that)";
+const string QueryParser::freeString = "(\\S+)";
+const string QueryParser::designEntity = "(procedure|stmtLst|stmt|assign|call|while|if|variable|constant|prog_line|plus|minus|times)";
+const string QueryParser::relRef = "([Ff]ollows|[Ff]ollows\\*|[Mm]odifies|[Mm]odifies\\*|[Uu]ses|[Uu]ses\\*|[Pp]arent|[Pp]arent\\*|[Cc]alls|[Cc]alls\\*|[Nn]ext|[Nn]ext\\*|[Aa]ffects|[Aa]ffects\\*)";
+const string QueryParser::pattern = "(pattern)";
+
+string stringToLower(string word){
+	for(int i=0;i<(int)word.length();i++){
+		word[i] = tolower(word[i]);
+	}
+	return word;
+}
+
+QueryParser::QueryParser(){
 	selectStatement.clear();
 	synMap.clear();
 }
 
 bool QueryParser::parseDesignEntity(string query){
 	tr1::cmatch res;
-	tr1::regex rx("\\s*" + designEntity + "\\s+(\\s*" + varName + "\\s*,)*(\\s*" + varName + "\\s*)");
+	tr1::regex rx("\\s*" + designEntity + "\\s+(\\s*" + IDENT + "\\s*,)*(\\s*" + IDENT + "\\s*)");
     tr1::regex_match(query.c_str(), res, rx);
 	
 	if(res.size()==0){
@@ -37,7 +59,7 @@ bool QueryParser::parseDesignEntity(string query){
 	}
 	while(getline(istream,subQuery,',')){
 		tr1::cmatch subRes;
-		tr1::regex subrx("\\s*" + varName + "\\s*");
+		tr1::regex subrx("\\s*" + IDENT + "\\s*");
 		tr1::regex_match(subQuery.c_str(), subRes, subrx);
 		string variableName = subRes[1];
 		synMap.insert(make_pair(variableName, newType));
@@ -49,7 +71,7 @@ bool QueryParser::parseDesignEntity(string query){
 
 bool QueryParser::parseSelectOnly(string query){
 	tr1::cmatch res;
-	tr1::regex rx("\\s*" + select + "\\s+" + varName + "\\s*");
+	tr1::regex rx("\\s*" + select + "\\s+" + IDENT + "\\s*");
     tr1::regex_match(query.c_str(), res, rx);
 	if(res.size()==0){
 		return false;
@@ -64,7 +86,7 @@ bool QueryParser::parseSelectOnly(string query){
 bool QueryParser::parsePattern(string query){
 	tr1::cmatch res;
 
-	tr1::regex rx("\\s*" + select + "\\s+" + varName + "\\s+" + pattern + "\\s+" + varName + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
+	tr1::regex rx("\\s*" + select + "\\s+" + IDENT + "\\s+" + pattern + "\\s+" + IDENT + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
     tr1::regex_match(query.c_str(), res, rx);
 
 	if(res.size()==0){
@@ -77,7 +99,7 @@ bool QueryParser::parsePattern(string query){
 }
 bool QueryParser::parseRelational(string query){
 	tr1::cmatch res;
-	tr1::regex rx("\\s*" + select + "\\s+" + varName + "\\s+" + suchThat + "\\s+" + relRef + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
+	tr1::regex rx("\\s*" + select + "\\s+" + IDENT + "\\s+" + suchThat + "\\s+" + relRef + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
     tr1::regex_match(query.c_str(), res, rx);
 	if(res.size()==0){
 		return false;
@@ -91,7 +113,7 @@ bool QueryParser::parseRelational(string query){
 
 bool QueryParser::parseRelationalWithPattern(string query){
 	tr1::cmatch res;
-	tr1::regex rx("\\s*" + select + "\\s+" + varName + "\\s+" + suchThat + "\\s+" + relRef + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)" + "\\s+" + pattern + "\\s+" + varName + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
+	tr1::regex rx("\\s*" + select + "\\s+" + IDENT + "\\s+" + suchThat + "\\s+" + relRef + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)" + "\\s+" + pattern + "\\s+" + IDENT + "\\s*\\(\\s*" + freeString + "\\s*,\\s*" + freeString + "\\s*\\)\\s*");
     tr1::regex_match(query.c_str(), res, rx);
 	if(res.size()==0){
 		return false;
@@ -136,11 +158,11 @@ Query QueryParser::queryParse(string queryStr, bool &valid){
 		}
 	}
 	valid = true;
-	Query query = makeQuery(selectStatement, synMap);
+	Query query = constructAndValidateQuery(selectStatement, synMap, valid);
 	return query;
 }
 
-Query QueryParser::makeQuery(vector<string> v, unordered_map<string, TypeTable::SynType> map){
+Query QueryParser::constructAndValidateQuery(vector<string> v, unordered_map<string, TypeTable::SynType> map, bool &valid){
 	Query query;
 	query.setSelectedSyn(v.at(1));
 	query.setSynTable(map);
--- Team02/Code02/QueryProcessor/QueryParser.h
+++ Team02/Code02/QueryProcessor/QueryParser.h
@@ -13,14 +13,28 @@ using namespace std;
 
 class QueryParser{
 private:
+	//constant string
+	static const string MODIFIES;
+	static const string USES;
+	static const string CALLS;
+	static const string CALLSSTAR;
+	static const string PARENT;
+	static const string PARENTSTAR;
+	static const string FOLLOWS;
+	static const string FOLLOWSSTAR;
+	static const string NEXT;
+	static const string NEXTSTAR;
+	static const string AFFECTS;
+	static const string AFFECTSSTAR;
+
 	//regex string
-	string varName;
-	string select;
-	string suchThat;
-	string freeString;
-	string designEntity;
-	string relRef;
-	string pattern;
+	static const string IDENT;
+	static const string select;
+	static const string suchThat;
+	static const string freeString;
+	static const string designEntity;
+	static const string relRef;
+	static const string pattern;
 
 	//query class table
 	vector<string> selectStatement;
@@ -32,7 +46,7 @@ private:
 	bool parsePattern(string);
 	bool parseRelational(string);
 	bool parseRelationalWithPattern(string);
-	Query makeQuery(vector<string>, unordered_map<string, TypeTable::SynType>);
+	Query constructAndValidateQuery(vector<string>, unordered_map<string, TypeTable::SynType>,bool&);
 
 public:
 	QueryParser();
