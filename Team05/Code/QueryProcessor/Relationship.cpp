a0f93a0102f7f85f4299afd35ed1794b60a05029
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-09-14 01:22:33 +0800

    Relationship class revamp to include TokenType #44

--- Team05/Code/QueryProcessor/Relationship.cpp
+++ Team05/Code/QueryProcessor/Relationship.cpp
@@ -8,10 +8,61 @@ string stringToLower(string word){
 	return word;
 }
 
+
+//get Relationship Type
+Relationship::RelType Relationship::getRelType(string relStr){
+    if (relStr == "follows") {
+        return FOLLOWS;
+    } else if (relStr == "follows*"){
+        return FOLLOWSSTAR;
+    } else if (relStr == "parent"){
+        return PARENT;
+	} else if (relStr == "parent*") {
+	    return PARENTSTAR;
+    } else if (relStr == "modifies"){
+		return MODIFIES;
+	} else if (relStr=="uses"){
+		return USES;
+	} else if (relStr=="calls"){
+		return CALLS;
+	} else if (relStr=="calls*"){
+		return CALLSSTAR;
+	} else if (relStr=="next"){
+		return NEXT;
+	} else if (relStr=="next*"){
+		return NEXTSTAR;
+	} else if (relStr=="affects"){
+		return AFFECTS;
+	} else if (relStr=="affects*"){
+		return AFFECTSSTAR;
+	} else if (relStr=="with"){
+		return WITH;
+	} else if (relStr=="pattern"){
+		return PATTERN;
+	}
+}
+
+//get Relationship Type
+Relationship::TokenType Relationship::getTokenType(string tokenStr){
+	if (tokenStr == "_") {
+        return UNDERSCORE;
+    } else if (tokenStr == "identifier"){
+        return IDENTIFIER;
+    } else if (tokenStr == "synonym"){
+        return SYNONYM;
+	} else if (tokenStr == "integer") {
+	    return INTEGER;
+    } else {
+		return INVALID;
+	}
+}
+
+
 //constructor for normal clause
 Relationship::Relationship(string newRel, string newToken1, string newToken2)
 {
     relName = stringToLower(newRel);
+	relType = getRelType(relName);
     token1 = newToken1;
     token2 = newToken2;
 }
@@ -20,43 +71,31 @@ Relationship::Relationship(string newRel, string newToken1, string newToken2)
 Relationship::Relationship(string newRel, string newPatternSyn, string newToken1, string newToken2)
 {
     relName = stringToLower(newRel);
+	relType = getRelType(relName);
 	patternSyn = newPatternSyn;
     token1 = newToken1;
     token2 = newToken2;
 }
 
+//constructor for normal clause with type
+Relationship::Relationship(string newRel, string newToken1, TokenType newType1, string newToken2, TokenType newType2 )
+{
+    relName = stringToLower(newRel);
+	relType = getRelType(relName);
+    token1 = newToken1;
+	token1Type = newType1;
+    token2 = newToken2;
+	token2Type = newType2;
+}
 
-//get Relationship Type
+//getter implementation
+
+string Relationship::getRelName(){
+	return relName;
+}
+
 Relationship::RelType Relationship::getRelType(){
-     if (relName == "follows") {
-        return FOLLOWS;
-    } else if (relName == "follows*"){
-        return FOLLOWSSTAR;
-    } else if (relName == "parent"){
-        return PARENT;
-	} else if (relName == "parent*") {
-	    return PARENTSTAR;
-    } else if (relName == "modifies"){
-		return MODIFIES;
-	} else if (relName=="uses"){
-		return USES;
-	} else if (relName=="calls"){
-		return CALLS;
-	} else if (relName=="calls*"){
-		return CALLSSTAR;
-	} else if (relName=="next"){
-		return NEXT;
-	} else if (relName=="next*"){
-		return NEXTSTAR;
-	} else if (relName=="affects"){
-		return AFFECTS;
-	} else if (relName=="affects*"){
-		return AFFECTSSTAR;
-	} else if (relName=="with"){
-		return WITH;
-	} else if (relName=="pattern"){
-		return PATTERN;
-	}
+	return relType;
 }
 
 string Relationship::getToken1(){
@@ -71,8 +110,23 @@ string Relationship::getPatternSyn(){
 	return patternSyn;
 }
 
-string Relationship::getWithSyn(int index){
-	return withSyn[index];
+Relationship::TokenType Relationship::getToken1Type(){
+	return token1Type;
+}
+
+Relationship::TokenType Relationship::getToken2Type(){
+	return token2Type;
+}
+
+
+//setter implementation
+
+void Relationship::setRelName(string newRelName){
+	relName = newRelName;
+}
+
+void Relationship::setRelType(Relationship::RelType newType){
+	relType = newType;
 }
 
 void Relationship::setToken1(string tk1){
@@ -83,10 +137,14 @@ void Relationship::setToken2(string tk2){
 	token2 = tk2;
 }
 
-void Relationship::setPatternSyn(string newPatternSyn){
-	patternSyn = newPatternSyn;
+void Relationship::setToken1Type(Relationship::TokenType newType1){
+	token1Type = newType1;
 }
 
-void Relationship::setWithSyn(string newWithSyn,int index){
-	withSyn[index] = newWithSyn;
+void Relationship::setToken2Type(Relationship::TokenType newType2){
+	token1Type = newType2;
+}
+
+void Relationship::setPatternSyn(string newPatternSyn){
+	patternSyn = newPatternSyn;
 }
--- Team05/Code/QueryProcessor/Relationship.h
+++ Team05/Code/QueryProcessor/Relationship.h
@@ -9,30 +9,47 @@
 using namespace std;
 
 class Relationship
-{
+{	
+	public:
+		enum RelType{FOLLOWS, FOLLOWSSTAR, PARENT, PARENTSTAR, MODIFIES, USES, CALLS, CALLSSTAR, NEXT, NEXTSTAR, AFFECTS, AFFECTSSTAR, WITH, PATTERN};
+		static RelType getRelType(string);
+		enum TokenType{ SYNONYM, IDENTIFIER, INTEGER, UNDERSCORE, INVALID};
+		static TokenType getTokenType(string);
+
     private:
+		string relName;
+		RelType relType;
 		string patternSyn; //for pattern
-		string withSyn[2]; //there are two possible with syn
-        string relName;
         string token1;
         string token2;
+		TokenType token1Type;
+		TokenType token2Type;
 
     public:
-        enum RelType{FOLLOWS, FOLLOWSSTAR, PARENT, PARENTSTAR, MODIFIES, USES, CALLS, CALLSSTAR, NEXT, NEXTSTAR, AFFECTS, AFFECTSSTAR, WITH, PATTERN};
-
+		//constructor for clause, token1, token2
         Relationship(string, string, string);
+		//constructor for clause, patternSyn, token1, token2
 		Relationship(string, string, string, string);
-
-        RelType getRelType();
+		//constructor for clause, token1, token1type, token2, token2type
+		Relationship(string, string, TokenType, string ,TokenType);
+        
+		//getter
+		string getRelName();
+		RelType getRelType();
+		string getPatternSyn();
         string getToken1();
         string getToken2();
-		string getPatternSyn();
-		string getWithSyn(int);
+		TokenType getToken1Type();
+		TokenType getToken2Type();
 		
+		//setter
+		void setRelName(string);
+		void setRelType(RelType);
 		void setToken1(string);
 		void setToken2(string);
+		void setToken1Type(TokenType);
+		void setToken2Type(TokenType);
 		void setPatternSyn(string);
-		void setWithSyn(string,int);
 
 };
 
