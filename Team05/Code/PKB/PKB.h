4edeb170fa52de012c96ab3041a6b5079f357e5e
yolim <yohaneslim93@gmail.com>
2014-10-04 14:02:05 +0800

    Updated PKB.h for data hiding #10
    No method has been implemented yet

--- Team05/Code/PKB/PKB.h
+++ Team05/Code/PKB/PKB.h
@@ -108,6 +108,86 @@ public:
 	//! Set the new CFG Root.
 	void setCFGRoot(CFGNode* newCFGRoot);
 
+	//! Return the variable name given the variable index. If it is not found, return an empty string.
+	VARNAME getVarName (VARINDEX);
+	//! Return the variable index given the variable name. If it is not found, return an -1.
+	VARINDEX getVarIndex (VARNAME);
+	int getNumVar(); 
+	vector<VARINDEX> getAllVarIndex();
+
+	//! Return the procedure name given the procedure index. If it is not found, return an empty string.
+	PROCNAME getProcName (PROCINDEX);
+	//! Return the procedure index given the procedure name. If it is not found, return an -1.
+	PROCINDEX getProcIndex (PROCNAME);
+	int getNumProcedures(); 
+	vector<PROCINDEX> getAllProcIndexes();
+
+	//! Return the constant value given the constant index. If it is not found, return an empty string.
+	CONSTVALUE getConstValue (CONSTINDEX);
+	//! Return the constant index given the constant value. If it is not found, return an -1.
+	CONSTINDEX getConstIndex (CONSTVALUE);
+	vector<int> getAllConstValue();
+
+	//! Return the SynType given the statement number. If the statement number, return INVALID.
+	TypeTable::SynType getSynType(STMTNUM);
+	//! Return all the statement numbers given the SynType.
+	vector<STMTNUM> getAllStmts(TypeTable::SynType);
+	//! Return true if the given statement number has the given SynType.
+	bool isSynType(TypeTable::SynType, STMTNUM);
+	//! Return the last statement number of the program. Return -1 if there is no source code
+	STMTNUM getStmtRange(); //return first and last stmtNum push_back into the vector
+
+	//! If the Calls relationship between the two procedure names is true, return true. Otherwise, return false.
+	bool isCalls(PROCINDEX,PROCINDEX);
+	vector<PROCINDEX> getAllCalls(); 
+	vector<PROCINDEX> getCalls(PROCINDEX); 	
+	vector<PROCINDEX> getAllCalled(); 
+	vector<PROCINDEX> getCalled(PROCINDEX);
+
+	//! If the Follows relationship between the two statement names is true, return true. Otherwise, return false.
+	bool isFollows(STMTNUM, STMTNUM);	
+	STMTNUM getFollows(STMTNUM);	
+	STMTNUM getFollowedBy(STMTNUM); 
+	vector<STMTNUM> getAllFollows();	 //Select s1 such that getFollows(s1, s2)
+	vector<STMTNUM> getAllFollowedBy();
+
+	bool isParent(STMTNUM, STMTNUM);
+	vector<STMTNUM> getAllChildren(); 
+	vector<STMTNUM> getChildren(STMTNUM);
+	vector<STMTNUM> getAllParent();
+	STMTNUM getParent(STMTNUM); 
+
+	//! If the Modifies relationship between a statement number and a variable name is true, return true. Otherwise, return false.
+	bool isModifies(STMTNUM, VARINDEX);	//Select w such that Modifies(1, "y")	
+	vector<VARINDEX> getModified(STMTNUM);
+	vector<STMTNUM> getModifies(VARINDEX);
+	vector<VARINDEX> getAllModified();
+	vector<STMTNUM> getAllModifies();
+
+	bool isModifiesProc(STMTNUM, PROCINDEX);	//Select w such that Modifies(1, "y")	
+	vector<VARINDEX> getModifiedProc(STMTNUM);
+	vector<STMTNUM> getModifiesProc(PROCINDEX);
+	vector<PROCINDEX> getAllModifiedProc();
+	vector<STMTNUM> getAllModifiesProc();
+
+	bool isUses(STMTNUM, VARINDEX);	//Select w such that Modifies(1, "y")	
+	vector<VARINDEX> getUsed(STMTNUM);
+	vector<STMTNUM> getUses(VARINDEX);
+	vector<VARINDEX> getAllUsed();
+	vector<STMTNUM> getAllUses();
+
+	bool isUsesProc(STMTNUM, PROCINDEX);	//Select w such that Modifies(1, "y")	
+	vector<VARINDEX> getUsedProc(STMTNUM);
+	vector<STMTNUM> getUsesProc(PROCINDEX);
+	vector<PROCINDEX> getAllUsedProc();
+	vector<STMTNUM> getAllUsesProc();
+
+	bool isNext(STMTNUM, STMTNUM);
+	vector<STMTNUM> getNext(STMTNUM);
+	vector<STMTNUM> getPrevious(STMTNUM);
+	vector<STMTNUM> getAllNext();
+	vector<STMTNUM> getAllPrevious();
+
 	/// @cond
 	VarTable* getVarTable();
 	ProcTable* getProcTable();
