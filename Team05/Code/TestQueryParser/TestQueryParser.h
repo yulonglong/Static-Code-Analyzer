a81a67577037be676862be05495211038a2546c9
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-10-13 03:23:55 +0800

    Updated TestQueryParser.h

--- Team05/Code/TestQueryParser/TestQueryParser.h
+++ Team05/Code/TestQueryParser/TestQueryParser.h
@@ -31,6 +31,7 @@ class QueryParserTest : public CPPUNIT_NS::TestFixture{
 	CPPUNIT_TEST(testQueryMultiplePatternAndWith);
 	CPPUNIT_TEST(testQueryMultipleSuchThatAndPatternAndWith);
 	CPPUNIT_TEST(testQueryTupleModifies);
+	CPPUNIT_TEST(testQueryTupleUses);
 	CPPUNIT_TEST(testQueryValidationModifies1);
 	CPPUNIT_TEST(testQueryValidationModifies2);
 	CPPUNIT_TEST(testQueryValidationModifies3);
@@ -79,6 +80,7 @@ public:
 	void testQueryMultiplePatternAndWith();
 	void testQueryMultipleSuchThatAndPatternAndWith();
 	void testQueryTupleModifies();
+	void testQueryTupleUses();
 	void testQueryValidationModifies1();
 	void testQueryValidationModifies2();
 	void testQueryValidationModifies3();
