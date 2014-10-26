48d75d7781812ee3081e4749e5a45ff35b95f24a
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-10-27 02:19:51 +0800

    Run Autotester against iteration0 test cases #113

--- Team05/Tests/iteration0/auto.bat
+++ Team05/Tests/iteration0/auto.bat
@@ -5,7 +5,6 @@ AutoTester.exe Source2.txt Queries2-3.txt ResultQueries2-3.xml >StdoutQueries2-3
 AutoTester.exe Source3.txt Queries3-1.txt ResultQueries3-1.xml >StdoutQueries3-1.txt
 AutoTester.exe Source4.txt Queries4-1.txt ResultQueries4-1.xml >StdoutQueries4-1.txt
 AutoTester.exe Source4.txt Queries4-2.txt ResultQueries4-2.xml >StdoutQueries4-2.txt
-AutoTester.exe Source4.txt Queries4-3.txt ResultQueries4-3.xml >StdoutQueries4-3.txt
 AutoTester.exe Source5.txt Queries5-1.txt ResultQueries5-1.xml >StdoutQueries5-1.txt
 AutoTester.exe Source5.txt Queries5-2.txt ResultQueries5-2.xml >StdoutQueries5-2.txt
 AutoTester.exe Source6.txt Queries6-1.txt ResultQueries6-1.xml >StdoutQueries6-1.txt
--- Team05/Tests/iteration1/auto.bat
+++ Team05/Tests/iteration1/auto.bat
@@ -1,4 +1,4 @@
-AutoTester.exe Source1A.txt QBasic1A-with-clause.txt ResultQBasic1A-with-clause.xml >StdoutQBasic1F-invalid-queries.txt
+AutoTester.exe Source1A.txt QBasic1A-with-clause.txt ResultQBasic1A-with-clause.xml >StdoutQBasic1A-with-clause.txt
 AutoTester.exe Source1B.txt QBasic1B-calls.txt ResultQBasic1B-calls.xml >StdoutQBasic1B-calls.txt
 AutoTester.exe Source1B.txt QBasic1C-calls-star.txt ResultQBasic1C-calls-star.xml >StdoutQBasic1C-calls-star.txt
 AutoTester.exe Source1D.txt QBasic1D-modifies-proc.txt ResultQBasic1D-modifies-proc.xml >StdoutQBasic1D-modifies-proc.txt
