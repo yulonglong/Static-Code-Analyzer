551bc5436881ab04e4f15910990ed74bbe6755b7
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-10-26 18:22:36 +0800

    Added automatic batch file to run testing #113

---
+++ Team05/Tests/iteration1/auto.bat
@@ -0,0 +1,7 @@
+AutoTester.exe Source1A.txt QBasic1A-with-clause.txt ResultQBasic1A-with-clause.xml >StdoutQBasic1F-invalid-queries.txt
+AutoTester.exe Source1B.txt QBasic1B-calls.txt ResultQBasic1B-calls.xml >StdoutQBasic1B-calls.txt
+AutoTester.exe Source1B.txt QBasic1C-calls-star.txt ResultQBasic1C-calls-star.xml >StdoutQBasic1C-calls-star.txt
+AutoTester.exe Source1D.txt QBasic1D-modifies-proc.txt ResultQBasic1D-modifies-proc.xml >StdoutQBasic1D-modifies-proc.txt
+AutoTester.exe Source1D.txt QBasic1E-uses-proc.txt ResultQBasic1E-uses-proc.xml >StdoutQBasic1E-uses-proc.txt
+AutoTester.exe Source1D.txt QBasic1F-invalid-queries.txt ResultQBasic1F-invalid-queries.xml >StdoutQBasic1F-invalid-queries.txt
+PAUSE
---
+++ Team05/Tests/iteration3/auto.bat
@@ -0,0 +1,8 @@
+AutoTester.exe Source3A.txt QBasic3A-affects.txt ResultQBasic3A-affects.xml >StdoutQBasic3A-affects.txt
+AutoTester.exe Source3A.txt QBasic3B-affects-star.txt ResultQBasic3B-affects-star.xml >StdoutQBasic3B-affects-star.txt
+AutoTester.exe Source3A.txt QBasic3C-invalid-queries.txt ResultQBasic3C-invalid-queries.xml >StdoutQBasic3C-invalid-queries.txt
+AutoTester.exe Source3A.txt QInterm3A-tuple.txt ResultQInterm3A-tuple.xml >StdoutQInterm3A-tuple.txt
+AutoTester.exe Source3A.txt QInterm3B-multiple-affects.txt ResultQInterm3B-multiple-affects.xml >StdoutQInterm3B-multiple-affects.txt
+AutoTester.exe Source3A.txt QInterm3C-multiple-affects-star.txt ResultQInterm3C-multiple-affects-star.xml >StdoutQInterm3C-multiple-affects-star.txt
+AutoTester.exe Source3A.txt QAdv3A-multiple-tuple.txt ResultQAdv3A-multiple-tuple.xml >StdoutQAdv3A-multiple-tuple.txt
+PAUSE
