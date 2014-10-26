83877db17b1570b44e00227d5b0155dfd465a019
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-10-26 16:23:30 +0800

    Run Autotester againts iteration0 test cases #113

---
+++ Team05/Tests/iteration0/auto.bat
@@ -0,0 +1,12 @@
+AutoTester.exe Source1.txt Queries1-1.txt ResultQueries1-1.xml >StdoutQueries1-1.txt
+AutoTester.exe Source2.txt Queries2-1.txt ResultQueries2-1.xml >StdoutQueries2-1.txt
+AutoTester.exe Source2.txt Queries2-2.txt ResultQueries2-2.xml >StdoutQueries2-2.txt
+AutoTester.exe Source2.txt Queries2-3.txt ResultQueries2-3.xml >StdoutQueries2-3.txt
+AutoTester.exe Source3.txt Queries3-1.txt ResultQueries3-1.xml >StdoutQueries3-1.txt
+AutoTester.exe Source4.txt Queries4-1.txt ResultQueries4-1.xml >StdoutQueries4-1.txt
+AutoTester.exe Source4.txt Queries4-2.txt ResultQueries4-2.xml >StdoutQueries4-2.txt
+AutoTester.exe Source4.txt Queries4-3.txt ResultQueries4-3.xml >StdoutQueries4-3.txt
+AutoTester.exe Source5.txt Queries5-1.txt ResultQueries5-1.xml >StdoutQueries5-1.txt
+AutoTester.exe Source5.txt Queries5-2.txt ResultQueries5-2.xml >StdoutQueries5-2.txt
+AutoTester.exe Source6.txt Queries6-1.txt ResultQueries6-1.xml >StdoutQueries6-1.txt
+PAUSE
