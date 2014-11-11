05073c05a177422d29e61bb1f1376d7971b78b88
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-11 13:42:54 +0800

    Updated QueryProcessor doxygen #155

--- Team05/Code/QueryProcessor/QueryProcessor.h
+++ Team05/Code/QueryProcessor/QueryProcessor.h
@@ -7,6 +7,20 @@
 #include "QueryEvaluator.h"
 #include "PKB.h"
 
+/*! \brief QueryProcessor is the brain of Static-Code Analyser
+ *  
+ *
+ * QueryProcessor is the main controller in the PQL.
+ * It will call relevant methods and sub-methods from:
+ * - QueryParser
+ * - QueryEvaluator
+ *
+ * It has to control and handle all the forwarded data (Query, Relationship) and pass them to the correct module/sub-module.
+ *
+ * \see QueryParser, QueryEvaluator, Relationship, Query
+ */
+
+//! Static method queryDriver drives all the program flow
 void queryDriver(std::string query, std::list<string>& result, PKB *pkb);
 vector<vector<int>> createTupleTable(unordered_map<int, vector<Pair>>, vector<Relationship>, unordered_map<string, int> *);
 
