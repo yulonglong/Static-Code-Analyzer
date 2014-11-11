0006274de786025ae1d3765d4b5d01dda3f672a5
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-11 13:33:14 +0800

    Updated Query class doxygen #155

--- Team05/Code/QueryProcessor/Query.h
+++ Team05/Code/QueryProcessor/Query.h
@@ -10,19 +10,52 @@
 
 using namespace std;
 
+/*! \brief A Query class which contains structured queries
+ *  
+ *
+ * A Query object will be created by QueryParser after validation and parsing the user query.
+ * It will then be forwarded to QueryEvaluator for evaluation and to answer they query.
+ * 
+ *
+ * 
+ * Query class contains the following relevant data:
+ * \code
+ * vector<string> selectedSyn; 
+ * vector<bool> selectedSynIsCallProcedure;
+ * string patternSyn;
+ * vector<Relationship> relVect;
+ * unordered_map<string, TypeTable::SynType> synTable;
+ * \endcode
+ *
+ * It contains the declared synonyms, selected synonyms, Relationship vector, etc.
+ * It has public functions to retrieve the data mentioned above.
+ *
+ * \see QueryParser, QueryEvaluator, Relationship
+ */
+
 class Query
 {
     public:
         Query();
+		//! get selected synonyms
 		vector<string> getSelectedSyn();
+		//! Set selected synonyms
 		void setSelectedSyn(vector<string>);
+		//! get special call synonym
 		vector<bool> getSelectedSynIsCallProcedure();
+		//! set special call synonym
 		void setSelectedSynIsCallProcedure(vector<bool>);
+		//! get pattern synonym
 		string getPatternSyn();
+		//! set pattern synonym
 		void setPatternSyn(string);
+		//! get a list of Relationship objects
         vector<Relationship> getRelVect();
+		//! get synonym table
 		unordered_map<string, TypeTable::SynType> getSynTable();
+		//! set synonym table
 		void setSynTable(unordered_map<string, TypeTable::SynType>);
+		//! add a Relationship object into the list
 		void addRelationship(Relationship);
 
 	private:
