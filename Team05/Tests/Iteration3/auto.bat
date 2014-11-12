1c814a73a2f1e2d1052fd4efa6eb1a7e740a6a84
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-12 22:02:06 +0800

    Include test back

---
+++ Team05/Tests/Iteration3/auto.bat
@@ -0,0 +1,6 @@
+AutoTester.exe Source3A.txt QBasic3A-affects.txt ResultQBasic3A-affects.xml >StdoutQBasic3A-affects.txt
+AutoTester.exe Source3A.txt QBasic3B-affects-star.txt ResultQBasic3B-affects-star.xml >StdoutQBasic3B-affects-star.txt
+AutoTester.exe Source3A.txt QInterm3A-tuple.txt ResultQInterm3A-tuple.xml >StdoutQInterm3A-tuple.txt
+AutoTester.exe Source3A.txt QAdv3A-multiple-tuple.txt ResultQAdv3A-multiple-tuple.xml >StdoutQAdv3A-multiple-tuple.txt
+AutoTester.exe Source3A.txt QAdv3B-optimization.txt ResultQAdv3B-optimization.xml >StdoutQAdv3B-optimization.txt
+PAUSE
---
+++ Team05/Tests/IterationX/auto.bat
@@ -0,0 +1,16 @@
+AutoTester.exe S1-simple.txt Q1-simple.txt R1-simple.xml >Stdout-Q1-simple.txt
+AutoTester.exe S2-assignment1.txt Q2-assignment1.txt R2-assignment1.xml >Stdout-Q2-assignment1.txt
+AutoTester.exe S4-example.txt Q4-example.txt R4-example.xml >Stdout-Q4-example.txt
+AutoTester.exe S5-basicTests.txt Q5-basicTests.txt R5-basicTests.xml >Stdout-Q5-basicTests.txt
+AutoTester.exe S6-pattern.txt Q6-pattern.txt R6-pattern.xml >Stdout-Q6-pattern.txt
+AutoTester.exe S7-cs3201.txt Q7-cs3201.txt R7-cs3201.xml >Stdout-Q7-cs3201.txt
+AutoTester.exe S8-with.txt Q8-with.txt R8-with.xml >Stdout-Q8-with.txt
+AutoTester.exe S9-with2.txt Q9-with2.txt R9-with2.xml >Stdout-Q9-with2.txt
+AutoTester.exe S10-complicatedTests.txt Q10-complicatedTests.txt R10-complicatedTests.xml >Stdout-Q10-complicatedTests.txt
+AutoTester.exe S11-affectsTests.txt Q11-affectsTests.txt R11-affectsTests.xml >Stdout-Q11-affectsTests.txt
+AutoTester.exe S12-complicatedAffects.txt Q12-complicatedAffects.txt R12-complicatedAffects.xml >Stdout-Q12-complicatedAffects.txt
+AutoTester.exe S13-next_affects.txt Q13-next_affects.txt R13-next_affects.xml >Stdout-Q13-next_affects.txt
+AutoTester.exe S14-newAffects.txt Q14-newAffects.txt R14-newAffects.xml >Stdout-Q14-newAffects.txt
+AutoTester.exe S15-demo.txt Q15-demo.txt R15-demo.xml >Stdout-Q15-demo.txt
+AutoTester.exe S16-tuple.txt Q16-tuple.txt R16-tuple.xml >Stdout-Q16-tuple.txt
+PAUSE
