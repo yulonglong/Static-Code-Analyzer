92034544a1791bdea10f50c09dcdc8fc0f03cb7b
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-13 02:32:25 +0800

    Refreshed Doxygen HTML Final

--- Doxygen_documentation/html/search/all_b.js
+++ Doxygen_documentation/html/search/all_b.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['query',['Query',['../class_query.html',1,'']]],
   ['queryevaluator',['QueryEvaluator',['../class_query_evaluator.html',1,'QueryEvaluator'],['../class_query_evaluator.html#a861e04948c25774d74a7dcb4137864aa',1,'QueryEvaluator::QueryEvaluator()']]],
-  ['queryevaluatortest',['QueryEvaluatorTest',['../class_query_evaluator_test.html',1,'']]],
   ['queryparse',['queryParse',['../class_query_parser.html#a12db252feb56df9af4bd5fb415cafb99',1,'QueryParser']]],
   ['queryparser',['QueryParser',['../class_query_parser.html',1,'']]],
   ['queryparsertest',['QueryParserTest',['../class_query_parser_test.html',1,'']]],
--- Doxygen_documentation/html/search/classes_a.js
+++ Doxygen_documentation/html/search/classes_a.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['query',['Query',['../class_query.html',1,'']]],
   ['queryevaluator',['QueryEvaluator',['../class_query_evaluator.html',1,'']]],
-  ['queryevaluatortest',['QueryEvaluatorTest',['../class_query_evaluator_test.html',1,'']]],
   ['queryparser',['QueryParser',['../class_query_parser.html',1,'']]],
   ['queryparsertest',['QueryParserTest',['../class_query_parser_test.html',1,'']]],
   ['queueitem',['QueueItem',['../class_queue_item.html',1,'']]]
