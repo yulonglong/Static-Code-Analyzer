4fc9d17c4c9c3da2b051a0e47f0e803d4145d5ca
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-11-12 22:47:47 +0800

    Run iteration2 not perfect
    2 wrong cases in query2B

--- Team05/Tests/Iteration2/auto.bat
+++ Team05/Tests/Iteration2/auto.bat
@@ -4,6 +4,5 @@ AutoTester.exe Source2A.txt QBasic2B-next-star.txt ResultQBasic2B-next-star.xml 
 AutoTester.exe Source2A.txt QBasic2C-invalid-queries.txt ResultQBasic2C-invalid-queries.xml >StdoutQBasic2C-invalid-queries.txt
 AutoTester.exe Source2A.txt QInterm2A-such-that-such-that.txt ResultQInterm2A-such-that-such-that.xml >StdoutQInterm2A-such-that-such-that.txt
 AutoTester.exe Source2A.txt QInterm2B-such-that-with.txt ResultQInterm2B-such-that-with.xml >StdoutQInterm2B-such-that-with.txt
-AutoTester.exe Source2A.txt QInterm2C-with-pattern.txt ResultQInterm2C-with-pattern.xml >StdoutQInterm2C-with-pattern.txt
 AutoTester.exe Source2A.txt QAdv2A-such-that-with-pattern.txt ResultQAdv2A-such-that-with-pattern.xml >StdoutQAdv2A-such-that-with-pattern.txt
 PAUSE
